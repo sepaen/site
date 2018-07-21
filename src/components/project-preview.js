4004d750bc99f6fe1ee2ff4e4854105c3294d034
jfalxa <jfalxa.sta@gmail.com>
2018-07-21 19:11:14 +0200

    scroll to project when clicking 'Next Project' on projects page

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import path from 'path'
 
 import Content from './content'
 import Flex from '../system/flex'
 import Cell from '../system/cell'
 import Text from '../system/text'
-import Link from '../system/link'
 import Image from '../system/image'
 
-const ProjectPreview = ({ project }) => (
-  <Flex flex={1} align="center">
+const ProjectPreview = ({ project, ...props }) => (
+  <Flex
+    {...props}
+    id={'anchor-' + project.frontmatter.slug}
+    flex="0 0 100%"
+    alignItems="center"
+    p={20}
+  >
     <Content height="66%">
       <Cell gridColumn="1" flexDirection="column" mr={4}>
         <Text children={project.frontmatter.title} />
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
+import ScrollToHash from '../components/scroll-to-hash'
 import ProjectPreview from '../components/project-preview'
+import Link from '../system/link'
 
 export const query = graphql`
   query ProjectsQuery {
@@ -12,7 +14,10 @@ export const query = graphql`
       }
     }
 
-    allMarkdownRemark {
+    allMarkdownRemark(
+      filter: { frontmatter: { type: { eq: "project" } } },
+      sort: { fields: frontmatter___date }
+    ) {
       edges {
         node {
           frontmatter {
@@ -28,11 +33,42 @@ export const query = graphql`
     }
   }
 `
+function next(location, projects=[]) {
+  const hash = location.hash.slice(1)
 
-const ProjectsPage = ({ data }) => (
-  <Layout title={data.site.siteMetadata.title}>
-    <ProjectPreview project={data.allMarkdownRemark.edges[1].node} />
-  </Layout>
-)
+  const index = (hash.length > 0)
+    ? projects.findIndex(({ node }) => node.frontmatter.slug === hash)
+    : 0
 
-export default ProjectsPage;
+  const nextProject = projects[(index + 1) % projects.length].node
+  return `/projects#${nextProject.frontmatter.slug}`
+}
+
+const ProjectsPage = ({ data, location }) => {
+  const title = data.site.siteMetadata.title
+  const projects = data.allMarkdownRemark.edges
+
+  return (
+    <Layout title={title}>
+      <ScrollToHash location={location}>
+        {projects.map(({ node }) => (
+          <ProjectPreview
+            key={node.frontmatter.slug}
+            project={node}
+          />
+        ))}
+      </ScrollToHash>
+
+      <Link
+        href={next(location, projects)}
+        children="Next Project"
+        position="fixed"
+        bottom={20}
+        right={20}
+      />
+    </Layout>
+  )
+}
+
+export default ProjectsPage
+
