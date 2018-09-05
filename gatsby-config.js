afc7cee971b6fd01893b8345138a74bb1b0b3843
jfalxa <jfalxa.sta@gmail.com>
2018-09-05 18:36:12 +0200

    added color widget in project cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         enableIdentityWidget: true,
+        modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     'gatsby-plugin-styled-components',
---
+++ src/cms/cms.js
@@ -0,0 +1,4 @@
+import CMS from 'netlify-cms'
+import * as ColorWidget from 'netlify-cms-widget-color'
+
+CMS.registerWidget('color', ColorWidget.Control)
